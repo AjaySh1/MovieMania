364d38bce7042e3a2055cb3c78feb83bd4c31ee3
Ajay Sharma <124371020+AjaySh1@users.noreply.github.com>
2024-07-04 21:55:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3ba758977c84617b0c5934c60b2a67b4&page=';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=';
-const WATCH_PROVIDERS_API = (id) => `https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=3fd2be6f0c70a2a598f084ddfb75487c`;
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3ba758977c84617b0c5934c60b2a67b4&query=';
+const WATCH_PROVIDERS_API = (id) => https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=3ba758977c84617b0c5934c60b2a67b4;
 
 const main = document.getElementById('main');
 const form = document.getElementById('form');
@@ -11,7 +11,7 @@ var prev_url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 
 // Function for fetching movies
 const fetchMovies = async (page, url) => {
-    const response = await fetch(url + `${page}`);
+    const response = await fetch(url + ${page});
     const data = await response.json();
     return data.results;
 };
@@ -59,7 +59,7 @@ async function showMovies(movies) {
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
 
-        movieEl.innerHTML = `
+        movieEl.innerHTML = 
             <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie-info">
                 <h3>${title}</h3>
@@ -70,7 +70,7 @@ async function showMovies(movies) {
                 ${overview}
                 ${watchProvidersHTML}
             </div>
-        `;
+        ;
         main.appendChild(movieEl);
     }
 }
@@ -78,17 +78,17 @@ async function showMovies(movies) {
 // Function to generate HTML for watch providers
 function getWatchProvidersHTML(watchProviders) {
   if (!watchProviders || !watchProviders.US || !watchProviders.US.flatrate) {
-    return `<div class="watch-providers" style="color:black;"><h4 style="color:#960606;text-decoration:underline;">Watch on:</h4>*Not available on any platform</div>`;
+    return <div class="watch-providers" style="color:black;"><h4 style="color:#960606;text-decoration:underline;">Watch on:</h4>*Not available on any platform</div>;
   }
   const providers = watchProviders.US.flatrate || [];
   const providersHTML = providers.map(provider => {
       if (provider.logo_path) {
-          return `<img src="https://image.tmdb.org/t/p/w92${provider.logo_path}" title="${provider.provider_name}" style="width: 40px; height: 40px; margin-right: 7px;">`;
+          return <img src="https://image.tmdb.org/t/p/w92${provider.logo_path}" title="${provider.provider_name}" style="width: 40px; height: 40px; margin-right: 7px;">;
       } else {
-          return `<span style="margin-right: auto; font-size: 14px;">${provider.provider_name}</span>`;
+          return <span style="margin-right: auto; font-size: 14px;">${provider.provider_name}</span>;
       }
   }).join('');
-  return providers.length > 0 ? `<div class="watch-providers"><h4 style="color:#960606;text-decoration:underline;">Watch on:</h4>${providersHTML}</div>` : '';
+  return providers.length > 0 ? <div class="watch-providers"><h4 style="color:#960606;text-decoration:underline;">Watch on:</h4>${providersHTML}</div> : '';
 }
 
 // Function for generating class based on rating
@@ -104,29 +104,34 @@ function getClassByRate(vote) {
 
 // Event listener for search form
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    
 
-    const searchTerm = search.value;
+    e.preventDefault()
 
-    if (searchTerm && searchTerm !== '') {
-        getMovies(SEARCH_API + searchTerm);
+    const searchTerm = search.value
 
-        search.value = '';
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
     } else {
-        window.location.reload();
+        window.location.reload()
     }
+
 });
 
 // JS for sidebar toggle
-var flag = true;
+var flag=true;
 document.getElementById('sidebar-toggle').addEventListener('click', function () {
-    if (flag) {
-        this.innerHTML = "&#10005;";
-        flag = false;
-    } else {
-        flag = true;
-        this.innerHTML = "☰";
-    }
+    if(flag)
+        {
+            this.innerHTML="&#10005;";
+            flag=false;
+        }
+        else
+        {  flag=true;
+            this.innerHTML="☰";
+        }
     document.body.classList.toggle('sidebar-visible');
 });
 
@@ -165,17 +170,17 @@ const handleLinkClick = (url, element) => {
 // JavaScript for dropdown content
 document.getElementById('revenue').addEventListener('click', function (event) {
     event.preventDefault();
-    handleLinkClick('https://api.themoviedb.org/3/discover/movie?sort_by=revenue.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=', this);
+    handleLinkClick('https://api.themoviedb.org/3/discover/movie?sort_by=revenue.desc&api_key=3ba758977c84617b0c5934c60b2a67b4&page=', this);
     prev_url = 'https://api.themoviedb.org/3/discover/movie?sort_by=revenue.desc';
 });
 document.getElementById('popularity').addEventListener('click', function (event) {
     event.preventDefault();
-    handleLinkClick('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=', this);
+    handleLinkClick('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3ba758977c84617b0c5934c60b2a67b4&page=', this);
     prev_url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc';
 });
 document.getElementById('rating').addEventListener('click', function (event) {
     event.preventDefault();
-    handleLinkClick('https://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=', this);
+    handleLinkClick('https://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&api_key=3ba758977c84617b0c5934c60b2a67b4&page=', this);
     prev_url = 'https://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc';
 });
 document.getElementById('popularity').classList.add('active');
@@ -219,11 +224,13 @@ document.querySelectorAll('#dropdown-content2 a').forEach((link) => {
 
         let genreUrl;
         if (genreId === '111') {
-            genreUrl = `${prev_url}&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=`;
+            genreUrl = ${prev_url}&api_key=3ba758977c84617b0c5934c60b2a67b4&page=;
         } else {
-            genreUrl = `${prev_url}&with_genres=${genreId}&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=`;
+            genreUrl = ${prev_url}&with_genres=${genreId}&api_key=3ba758977c84617b0c5934c60b2a67b4&page=;
         }
     handleGenreLinkClick2(genreUrl, this);
   
 });
 });
+
+// js for genre part ends.
